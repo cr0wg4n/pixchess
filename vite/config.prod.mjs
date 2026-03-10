088b2733cacaab96f2216abe417cda8081650c99
cr0wg4n <mcm.crw@gmail.com>
2026-03-10 00:20:19 -0400

    my initial setup

---
+++ vite/config.prod.mjs
@@ -0,0 +1,48 @@
+import process from 'node:process'
+import { defineConfig } from 'vite'
+
+function phasermsg() {
+  return {
+    name: 'phasermsg',
+    buildStart() {
+      process.stdout.write(`Building for production...\n`)
+    },
+    buildEnd() {
+      const line = '---------------------------------------------------------'
+      const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`
+      process.stdout.write(`${line}\n${msg}\n${line}\n`)
+
+      process.stdout.write(`✨ Done ✨\n`)
+    },
+  }
+}
+
+export default defineConfig({
+  base: './',
+  logLevel: 'warning',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+        },
+      },
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+  },
+  server: {
+    port: 8080,
+  },
+  plugins: [
+    phasermsg(),
+  ],
+})
